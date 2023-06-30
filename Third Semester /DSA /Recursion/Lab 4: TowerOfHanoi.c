2f62c9afbd35af2fea460ce58f61a50254382338
Atullya <100409703+atullya@users.noreply.github.com>
2023-06-30 08:59:01 +0545

    Update Lab 4: TowerOfHanoi.c

--- Third Semester /DSA /Recursion/Lab 4: TowerOfHanoi.c
+++ Third Semester /DSA /Recursion/Lab 4: TowerOfHanoi.c
@@ -1,15 +1,16 @@
-// Online C compiler to run C program online
+
 #include <stdio.h>
 int i=1;
 int TOH(int n, char A, char B, char C){
     if(n>0){
         TOH(n-1,A,C,B);
-        printf("Move %d disk from %c to %c \n ",n, A,C);
+        printf("%d Move %d disk from %c to %c \n ",i++,n, A,C);
         TOH(n-1,B,A,C);
     }
 }
 
 int main() {
+    system ("clear");
    int n;
    printf("Enter the number of disk:");
    scanf("%d",&n);
