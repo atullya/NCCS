14f573af37a26e21dfa1ebfd63d870bf6b795a1a
Atullya <100409703+atullya@users.noreply.github.com>
2023-09-28 12:59:33 +0545

    d

--- Third Semester /DSA /LinkedList/Lab 1_ ImplemenationOfListUsingArray.c
+++ Third Semester /DSA /LinkedList/Lab 1_ ImplemenationOfListUsingArray.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #define MAX_SIZE 5
 
-struct list
-{
+struct list {
     int data[MAX_SIZE];
     int size;
 };
@@ -16,47 +15,42 @@ void deleteElement(listType *);
 void search(listType *);
 void modify(listType *);
 
-int main()
-{
-   // system("clear");
+int main() {
     listType list;
     list.size = 0;
     int choice;
     printf("\n1. Insert\n2. Delete\n3. Search\n4. Modify\n5. Print\n6. Exit\n");
-    do
-    {
+    do {
         printf("\nEnter your choice : ");
         scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            insert(&list);
-            break;
-        case 2:
-            deleteElement(&list);
-            break;
-        case 3:
-            search(&list);
-            break;
-        case 4:
-            modify(&list);
-            break;
-        case 5:
-            printList(&list);
-            break;
-        case 6:
-            printf("Exiting...\n");
-            break;
-        default:
-            printf("Invalid choice\n");
+        switch (choice) {
+            case 1:
+                insert(&list);
+                break;
+            case 2:
+                deleteElement(&list);
+                break;
+            case 3:
+                search(&list);
+                break;
+            case 4:
+                modify(&list);
+                break;
+            case 5:
+                printList(&list);
+                break;
+            case 6:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
         }
     } while (choice != 6);
 }
 
-void insert(listType *list)
-{
-    if (list->size == MAX_SIZE)
-    {
+void insert(listType *list) {
+    if (list->size == MAX_SIZE) {
         printf("List is full\n");
         return;
     }
@@ -64,18 +58,14 @@ void insert(listType *list)
     int data, pos;
     printf("Enter data: ");
     scanf("%d", &data);
-    printf("Enter position [1 - %d]: ", MAX_SIZE);
+    printf("Enter position [1 - %d]: ", list->size + 1);
     scanf("%d", &pos);
 
-    if (pos > MAX_SIZE || pos < 1)
-    {
+    if (pos > list->size + 1 || pos < 1) {
         printf("Invalid position\n");
         return;
-    }
-    else
-    {
-        for (int i = list->size; i >= pos; i--)
-        {
+    } else {
+        for (int i = list->size; i >= pos; i--) {
             list->data[i] = list->data[i - 1];
         }
         list->data[pos - 1] = data;
@@ -84,59 +74,48 @@ void insert(listType *list)
     }
 }
 
-void deleteElement(listType *list)
-{
-    if (list->size == 0)
-    {
+void deleteElement(listType *list) {
+    if (list->size == 0) {
         printf("List is empty\n");
         return;
     }
     int pos, value;
     printf("Enter position [1 - %d]: ", list->size);
     scanf("%d", &pos);
-    if (pos > list->size || pos < 1)
-    {
+    if (pos > list->size || pos < 1) {
         printf("Invalid position\n");
         return;
-    }
-    else
-    {
+    } else {
         value = list->data[pos - 1];
-        for (int i = pos - 1; i < list->size; i++)
-        {
+        for (int i = pos - 1; i < list->size - 1; i++) {
             list->data[i] = list->data[i + 1];
         }
         list->size--;
     }
     printf("The value %d at position %d is deleted\n", value, pos);
 }
-void modify(listType *list)
-{
+
+void modify(listType *list) {
     int value, pos;
     printf("Enter position [1 - %d]: ", list->size);
     scanf("%d", &pos);
     printf("Enter new value: ");
     scanf("%d", &value);
-    if (pos > list->size || pos < 1)
-    {
+    if (pos > list->size || pos < 1) {
         printf("Invalid position\n");
         return;
-    }
-    else
-    {
+    } else {
         list->data[pos - 1] = value;
     }
     printf("The value at position %d is modified to %d\n", pos, value);
 }
-void search(listType *list)
-{
+
+void search(listType *list) {
     int value;
     printf("Enter value to search: ");
     scanf("%d", &value);
-    for (int i = 0; i <= list->size; i++)
-    {
-        if (list->data[i] == value)
-        {
+    for (int i = 0; i < list->size; i++) {
+        if (list->data[i] == value) {
             printf("The value found at position %d\n", i + 1);
             return;
         }
@@ -144,17 +123,14 @@ void search(listType *list)
     printf("The value is not found\n");
 }
 
-void printList(listType *list)
-{
-    if (list->size == 0)
-    {
+void printList(listType *list) {
+    if (list->size == 0) {
         printf("List is empty\n");
         return;
     }
     printf("List: ");
-    for (int i = 0; i < list->size; i++)
-    {
-        printf("%d\t ", list->data[i]);
+    for (int i = 0; i < list->size; i++) {
+        printf("%d\t", list->data[i]);
     }
     printf("\n");
 }
