ace716b5aba17db7ba1fa7e31b4f48594914404a
Atullya <100409703+atullya@users.noreply.github.com>
2024-06-06 18:47:06 +0545

    Create BubbleSort.cpp

---
+++ Fifth Semester/DAA/Sorting/BubbleSort.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int count = 0;
+
+void BubbleSort(int A[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(A[j]>A[j+1])
+            {
+                int t= A[j];
+                A[j]=A[j+1];
+                A[j+1]=t;
+                
+            }
+            
+            count =count+8;
+            
+        }
+        count = count+4;
+    }    
+}
+
+int main()
+{
+    
+int a[9] = {110,2,22,44,819,30,10,1,210};
+
+
+
+cout<<"Before sorting: \n";
+   for(int i=0 ;i<9 ;i++)
+        {
+    cout<<a[i]<<"\t" ;
+    
+    
+        }
+        
+    cout<<endl;
+    
+BubbleSort(a,9);
+
+cout<<"After sorting: \n";
+ for(int i=0 ;i<9 ;i++)
+        {
+    cout<<a[i]<<"\t" ;
+        }
+cout<<endl;        
+cout<<"No. of Steps required : "<<count;        
+        
+return 0;
+
+}
